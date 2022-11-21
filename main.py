82ce6880899b566f69fe1d1c281e4f2883f64335
Hpdvn <hugop230800@gmail.com>
2022-11-21 10:52:42 +0100

    first song search

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from lyricsgenius import Genius
+from config import ACCESS_TOKEN
 
-genius = Genius(secrets.Genius_API_token)
-artist = genius.search_artist("1.Cuz", max_songs=3, sort="title")
+genius = Genius(ACCESS_TOKEN)
+
+artist = genius.search_artist(artist_name="1.Cuz", max_songs=3, sort="title")
 print(artist.songs)
